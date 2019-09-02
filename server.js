67d8483783a5695e31cdfedb1468d91e207d8de0
ashort <andrews228@gmail.com>
2019-09-02 11:20:46 -0400

    test heroku deploy

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 var http = require('http').Server(app);
 
 // Serve up content from public directory
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
 http.listen(process.env.PORT || 3000);
