96bf339262b88a5b8a06e928996e8ebcb598a95a
ashort <andrews228@gmail.com>
2019-09-21 09:59:49 -0400

    removed dependency on local copies of position/zoom

--- public/js/map.js
+++ public/js/map.js
@@ -5,15 +5,7 @@ var geomap = function(elem) {
     var wgs84 = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
     var mercator   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
     
-    this.position       = new OpenLayers.LonLat(-86.16,39.76).transform( wgs84, mercator);
-    this.zoom           = 9;
-    
     this.map.addLayer(this.mapnik);
-    this.map.setCenter(this.position, this.zoom );
-
-    //var markers = new OpenLayers.Layer.Markers( "Markers" );
-    
-    //this.map.addLayer(markers);
     
     var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
     this.map.addLayer(vectorLayer);
@@ -47,7 +39,6 @@ var geomap = function(elem) {
 
     this.addMarker = function(lat, lng, featureImg, featureTitle) {
         
-    
         // Define markers as "features" of the vector layer:
         var feature = new OpenLayers.Feature.Vector(
                 new OpenLayers.Geometry.Point( lng, lat ).transform(wgs84, this.map.getProjectionObject()),
@@ -55,29 +46,18 @@ var geomap = function(elem) {
                 {externalGraphic: 'img/marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
             );    
         vectorLayer.addFeatures(feature);
-
-
-        /*
-        var lonLat = new OpenLayers.LonLat( lng, lat )
-        .transform(wgs84,
-            this.map.getProjectionObject() // to Spherical Mercator Projection
-        );
-
-        markers.addMarker(new OpenLayers.Marker(lonLat));
-        */
     }
 
     this.setCenter = function(lat, lng, zoom) {
-        this.position = new OpenLayers.LonLat(lng,lat).transform( wgs84, mercator);
-        this.zoom = zoom || this.map.getZoom();;
+        var position = new OpenLayers.LonLat(lng,lat).transform( wgs84, mercator);
+        zoom = zoom || this.map.getZoom();
         
-        this.map.setCenter(this.position, this.zoom);
-
-        
+        this.map.setCenter(position, zoom);
     };
 
     this.getCenter = function() {
-        return this.position.transform(mercator, wgs84);
+        var center = this.map.center;
+        return new OpenLayers.LonLat(center.lon,center.lat).transform( mercator, wgs84 );
     }
 
     this.onmapclick = function(cb) {
@@ -113,6 +93,8 @@ var geomap = function(elem) {
             );
            // handler.activate();
     }
+
+    this.setCenter(39.76, -86.16, 9 );
 
     
 };
