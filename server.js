a4578b9a1792f8513eaa5e655141b75bacf94971
ashort <andrews228@gmail.com>
2019-09-21 22:31:29 -0400

    add address search

--- server.js
+++ server.js
@@ -8,6 +8,25 @@ app.use(express.static(__dirname + '/public/'));
 
 var router = express.Router();
 
+router.get('/addresssearch', function(req, res) {
+    
+    var q = req.query.q;
+    var webreq = unirest("GET", "https://nominatim.openstreetmap.org/search?q=" + q + "&format=json");
+    
+    webreq.headers({
+        "User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0"
+    })
+    webreq.end(function (webres) {
+        if (webres.error) {
+            console.log(webres.error);
+            res.json(webres.error);
+            return;
+        }
+
+        res.json(webres.body);
+    });
+})
+
 router.get('/list', function(req, res) {
     var nearby = req.query.nearby;
 	var webreq = unirest("GET", "https://webcamstravel.p.rapidapi.com/webcams/list/nearby=" + nearby);
@@ -26,7 +45,8 @@ router.get('/list', function(req, res) {
     webreq.end(function (webres) {
         if (webres.error) {
             console.log(webres.error);
-            res.json(webres.error)
+            res.json(webres.error);
+            return;
         }
 
         res.json(webres.body);
